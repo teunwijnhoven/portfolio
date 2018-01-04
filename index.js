1327799e75ac8882fd19d8d10f0da6a9a5a960f4
Unknown <teunie_@hotmail.com>
2018-01-04 14:14:56 +0100

    Update met datum en tijd

---
+++ index.js
@@ -0,0 +1,43 @@
+			function myFunction() {
+				var date, text;
+
+    			// Get the value of the input field with id="numb"
+    			date = document.getElementById("numb").value;
+
+    			countdown(date);
+
+    		}
+
+			function countdown(x){
+				var now = new Date();
+				var eventDate = new Date(isNaN(x));
+
+				var currentTime = now.getTime();
+				var eventTime = eventDate.getTime();
+
+				var remTime = eventTime - currentTime;
+
+				var s = Math.floor(remTime / 1000);
+				var m = Math.floor(s / 60);
+				var h = Math.floor(m / 60);
+				var d = Math.floor(h / 24);
+
+				h %= 24;
+				m %= 60;
+				s %= 60;
+
+				h = (h < 10) ? "0" + h : h;
+				m = (m < 10) ? "0" + m : m;
+				s = (s < 10) ? "0" + s : s;
+
+				document.getElementById("dagen").textContent = d;
+				document.getElementById("dagen").innetText = d;
+
+				document.getElementById("uren").textContent = h;
+				document.getElementById("minuten").textContent = m;
+				document.getElementById("seconden").textContent = s;
+
+				setTimeout(countdown, 1000);
+			}
+
+			countdown();
